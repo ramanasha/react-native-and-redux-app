d4f4f44523ea350559a086a107d0882c9e2606d5
Gui Gatti Dev <gattiwhoomps@hotmail.com>
2017-09-17 21:32:24 -0300

    Hello app and create json of winnipeg restaurant

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+// Import
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { App } from './src/App';
 
-export default class app extends Component {
+// Component
+class app extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <App />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
+// Render
 AppRegistry.registerComponent('app', () => app);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Import
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+// Component
+export default class App extends Component {
+  render() {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+  }
+}
